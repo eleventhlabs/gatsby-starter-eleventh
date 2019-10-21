3f1e9f9848e1cd03b4d4c5970837fbf5aab1c093
Stephen Chen <hello@stephenc.me>
2019-10-21 14:53:39 -0400

    Adds Segment plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-emotion',
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-segment-js',
+      options: {
+        prodKey: '',
+        devKey: '',
+        trackPage: true
+      }
+    }
   ]
 }
